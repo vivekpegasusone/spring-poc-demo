96372eb1ea8bc57f3d2239797ae22f46c8fca907
Vivek Singh <40670506+vivekpegasusone@users.noreply.github.com>
2018-06-29 21:56:36 +0530

    Update HomeController.java

--- src/main/java/com/spring/demo/controller/HomeController.java
+++ src/main/java/com/spring/demo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.spring.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,16 @@ import com.spring.demo.form.Sentence;
 
 @Controller
 public class HomeController {
+	
+	/**
+	 * Initializing logger for this class
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping("/")
 	public String showMessage(Model model) {
+		LOGGER.info("Landing page");
 		model.addAttribute("sentence", new Sentence());
  		return "index";
 	}
 }
-
